8433ca7408e718ce0ad86398389289f281369b5d
Alex <bumbull444@gmail.com>
2024-03-20 18:24:09 +0300

    First Prectice

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 'use strict';
 
-console.log('Hello world!');
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let Q1 = prompt('Один из последних просмотренных фильмов?', '');
+let Q2 = +prompt('На сколько оцените его?', '');
+let Q3 = prompt('Один из последних просмотренных фильмов?', '');
+let Q4 = +prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[Q1] = Q2;
+personalMovieDB.movies[Q3] = Q4;
+
+console.log(personalMovieDB);
+
