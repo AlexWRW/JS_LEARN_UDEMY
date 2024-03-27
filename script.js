1e6326bd904a44178c37d6f267db1443c1554782
Alex <bumbull444@gmail.com>
2024-03-27 20:58:32 +0300

    Second practice

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,13 +10,32 @@ const personalMovieDB = {
     privat: false
 };
 
-let Q1 = prompt('Один из последних просмотренных фильмов?', '');
-let Q2 = +prompt('На сколько оцените его?', '');
-let Q3 = prompt('Один из последних просмотренных фильмов?', '');
-let Q4 = +prompt('На сколько оцените его?', '');
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >=10 && personalMovieDB.count<=30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count>30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Ответ перепиши, пидор');
+}
 
-personalMovieDB.movies[Q1] = Q2;
-personalMovieDB.movies[Q3] = Q4;
+// let Q1 = prompt('Один из последних просмотренных фильмов?', '');
+// let Q2 = +prompt('На сколько оцените его?', '');
+// let Q3 = prompt('Один из последних просмотренных фильмов?', '');
+// let Q4 = +prompt('На сколько оцените его?', '');
 
-console.log(personalMovieDB);
+for (let i=0; i<2; i++) {
+    const Q1 = prompt('Один из последних просмотренных фильмов?', '');
+    const Q2 = +prompt('На сколько оцените его?', '');
+    if(Q1 != null && Q2 != null || Q1 != '' && Q2 != '' || Q1.length <50 && Q2 <50) {
+        personalMovieDB.movies[Q1] = Q2;
+    } else {
+        i--;
+    }
+}
+
+// personalMovieDB.movies[Q1] = Q2;
+// personalMovieDB.movies[Q3] = Q4;
+
 
